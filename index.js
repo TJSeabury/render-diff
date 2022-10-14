e6f9a11e3923d9ab6f021acb9d257fac511ea9a8
Tyler Seabury <tylerseabury@gmail.com>
2022-10-14 10:53:45 -0400

    refactored, added basic test

--- index.js
+++ index.js
@@ -1,58 +1,129 @@
 const fs = require( 'fs' );
 const puppeteer = require( 'puppeteer' );
+const cron = require( 'node-cron' );
+const path = require( 'path' );
 
-async function run () {
+
+const capture = async ( page, domain, sizes ) => {
+  await page.goto( `https://${domain}`,
+    {
+      waitUntil: 'networkidle0'
+    }
+  );
+
+  for ( const [
+    name,
+    {
+      width,
+      height
+    }
+  ] of Object.entries( sizes ) ) {
+    await page.setViewport( {
+      width: width,
+      height: height
+    } );
+    await page.reload( { waitUntil: 'networkidle0' } );
+    sizes[name].imageBuffer = await page.screenshot( {
+      fullPage: true,
+      type: 'png',
+      encoding: 'binary'
+    } );
+  }
+  return sizes;
+};
+
+const buildFileName = ( domain, size ) => `${domain}-${size.width}x${size.height}.png`;
+
+const saveMaster = ( domain, size ) => {
+  const filename = buildFileName( domain, size );
+  console.log( `Saving ${filename} ...`, size );
+  fs.writeFileSync( filename, size.imageBuffer );
+};
+
+const loadMaster = ( domain, size ) => {
+  return fs.readFileSync( buildFileName( domain, size ) );
+};
+
+const sizes = ( sizes ) => sizes.reduce( ( accum, [w, h] ) => ( {
+  ...accum,
+  [`${w}x${h}`]: {
+    width: w,
+    height: h
+  }
+} ), {} );
+
+const areBuffersEqual = ( bufferA, bufferB ) => Buffer.compare( bufferA, bufferB ) === 0;
+
+
+async function visitPages ( action ) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setCacheEnabled( false );
 
-  const capture = async ( page, domain, sizes ) => {
-    await page.goto( `https://${domain}`,
-      {
-        waitUntil: 'networkidle0'
-      }
+  action( page );
+
+  browser.close();
+}
+
+
+async function createMasterReferences ( page, domain, devices ) {
+
+  const data = await capture(
+    page,
+    domain,
+    devices
+  );
+
+  Object.entries( data ).map( ( [_, datum] ) => { saveMaster( domain, datum ); } );
+
+}
+
+async function createVerificationReferences () {
+  const data = await capture(
+    page,
+    domain,
+    devices
+  );
+
+  const pairedData = [
+    Object.entries( firstData ),
+    Object.entries( data )
+  ];
+
+
+  let diffs = [];
+  const safeLength = Math.min( pairedData[0], pairedData[1] );
+  for ( let i = 0; i < safeLength; ++i ) {
+    diffs[i] = areBuffersEqual(
+      pairedData[0][i].imageBuffer,
+      pairedData[1][i].imageBuffer
     );
+  }
 
-    for ( const [
-      name,
-      {
-        width,
-        height
-      }
-    ] of Object.entries( sizes ) ) {
-      await page.setViewport( {
-        width: width,
-        height: height
-      } );
-      await page.reload( { waitUntil: 'networkidle0' } );
-      sizes[name].imageBuffer = await page.screenshot( {
-        fullPage: true,
-        type: 'png',
-        encoding: 'binary'
-      } );
-    }
-    return sizes;
-  };
+  return [pairedData, diffs];
 
-  const buildFileName = ( domain, size ) => `${domain}-${size.width}x${size.height}.png`;
+}
 
-  const saveMaster = ( domain, size ) => {
-    const filename = buildFileName( domain, size );
-    console.log( `Saving ${filename} ...`, size );
-    fs.writeFileSync( filename, size.imageBuffer );
-  };
+const onReadError = ( err, data ) => {
+  if ( err ) throw err;
+  console.log( data );
+};
 
-  const loadMaster = ( domain, size ) => {
-    return fs.readFileSync( buildFileName( domain, size ) );
-  };
+async function run () {
 
-  const sizes = ( sizes ) => sizes.reduce( ( accum, [w, h] ) => ( {
-    ...accum,
-    [`${w}x${h}`]: {
-      width: w,
-      height: h
-    }
-  } ), {} );
+  // Testing
+  const bubbleBassBuffer = fs.readFileSync( path.resolve( './_test/bubble-bass.webp' ) );
+  const picklesBuffer = fs.readFileSync( path.resolve( './_test/pickles.webp' ) );
+  if ( !bubbleBassBuffer || !picklesBuffer ) return;
+  const testResult = areBuffersEqual( bubbleBassBuffer, picklesBuffer );
+  console.log( [
+    'Test - areBuffersEqual( bubbleBassBuffer, picklesBuffer ): ',
+    `Should be false: ${testResult}`
+  ] );
+  if ( testResult === true ) throw new Error( 'Buffer test failed.' );
+
+
+  // Let's get down to business~!
 
   const devices = sizes( [
     [2560, 1440],
@@ -64,104 +135,15 @@ async function run () {
 
   const domain = 'marketmentors.com';
 
-  const firstData = await capture(
-    page,
-    domain,
-    devices
-  );
+  visitPages( ( page ) => { createMasterReferences( page, domain, devices ); } );
 
-  Object.entries( firstData ).map( ( [_, datum] ) => { saveMaster( domain, datum ); } );
+  /* cron.schedule( '* * * * *', async function doScheduledTask() {
+      // console.log( 'running a task every minute' );
+  
+  
+  
+  } ); */
 
-  const secondData = await capture(
-    page,
-    domain,
-    devices
-  );
-
-  const pairedData = [
-    Object.entries( firstData ),
-    Object.entries( secondData )
-  ];
-
-  console.log( pairedData );
-
-
-  browser.close();
 }
 
 run();
-
-
-
-
-
-
-
-
-
-
-
-async function pageCoverage ( page, domain ) {
-  await Promise.all( [
-    page.coverage.startJSCoverage(),
-    page.coverage.startCSSCoverage()
-  ] );
-
-  await page.goto( `https://${domain}`,
-    {
-      waitUntil: 'networkidle0'
-    }
-  );
-
-  const [jsCoverage, cssCoverage] = await Promise.all( [
-    page.coverage.stopJSCoverage(),
-    page.coverage.stopCSSCoverage(),
-  ] );
-
-  console.log( {
-    coverage: {
-      jsCoverage: examineJSCoverage( jsCoverage ),
-      cssCoverage: examineCSSCoverage( cssCoverage )
-    }
-  } );
-}
-
-function examineJSCoverage ( jsCoverage ) {
-  const js_coverage = [...jsCoverage];
-  let js_used_bytes = 0;
-  let js_total_bytes = 0;
-  let covered_js = "";
-  for ( const entry of js_coverage[0] ) {
-    js_total_bytes += entry.text.length;
-    for ( const range of entry.ranges ) {
-      js_used_bytes += range.end - range.start - 1;
-      covered_js += entry.text.slice( range.start, range.end ) + "\n";
-    }
-  }
-
-  return [
-    `Total Bytes of JS: ${js_total_bytes}`,
-    `Used Bytes of JS: ${js_used_bytes}`,
-    `Ratio: ${js_used_bytes / js_total_bytes}`
-  ];
-}
-
-function examineCSSCoverage ( cssCoverage ) {
-  const css_coverage = [...cssCoverage];
-  let css_used_bytes = 0;
-  let css_total_bytes = 0;
-  let covered_css = "";
-  for ( const entry of css_coverage[0] ) {
-    css_total_bytes += entry.text.length;
-    for ( const range of entry.ranges ) {
-      css_used_bytes += range.end - range.start - 1;
-      covered_css += entry.text.slice( range.start, range.end ) + "\n";
-    }
-  }
-
-  return [
-    `Total Bytes of CSS: ${css_total_bytes}`,
-    `Used Bytes of CSS: ${css_used_bytes}`,
-    `Ratio: ${css_used_bytes / css_total_bytes}`
-  ];
-}
